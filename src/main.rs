61f797b11fdc8785ca0b55f5a1c106c9bc976e5d
Geoff Johnson <geoff.jay@gmail.com>
2024-04-30 08:20:59 -0700

    feat: add subcommands

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,37 @@ use clap::{Parser, Subcommand};
 #[derive(Parser)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    name: String
+    re: String,
+
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+enum Commands {
+    Map(String),
+    Filter(String),
+    Reduce(String),
 }
 
 fn main() {
     let args = Args::parse();
   
-    println!("Hello, {}!", args.name);
+    match args.cmd {
+        Commands::Map(value) => cmd_map(value),
+        Commands::Filter(value) => cmd_filter(value),
+        Commands::Reduce(value) => cmd_reduce(value),
+    }
 }
 
+fn cmd_map(value: String) {
+    println!("Map: {}", value);
+}
+
+fn cmd_filter(value: String) {
+    println!("Filter: {}", value);
+}
+
+fn cmd_reduce(value: String) {
+    println!("Reduce: {}", value);
+}
